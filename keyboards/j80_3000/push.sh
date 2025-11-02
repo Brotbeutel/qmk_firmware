b58a6d5ae847b58d4569534a03e08b9c49c0e89e
Brotbeutel <jannik_schlueter@hotmail.de>
2025-11-02 15:42:14 +0100

    Auto-Commit

--- keyboards/j80_3000/push.sh
+++ keyboards/j80_3000/push.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
 # This is a script to automate the git commit function. When you're in this directory: C:/Users/Student/qmk_firmware/keyboards/j80_3000,
 # you can run this command: ./push.sh "Your message of changes here. (optional)"
 
-#!/bin/bash
 git add .
 git commit -m "${1:-Auto-Commit}"
 git push
